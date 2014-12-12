d9aed531a7ea9051833a1992650b9c4dda82d1d3
Peter <peterhsaxton@gmail.com>
2014-12-12 10:20:16 +0000

    use strategy module directly

--- lib/atheneum/strategy.rb
+++ lib/atheneum/strategy.rb
@@ -6,7 +6,7 @@ module Atheneum
     def self.find(name)
       nodule = constantize(name)
       begin
-        new const_get(nodule).new
+        const_get(nodule).new
       rescue NameError
         raise StrategyUndefined.new "Strategy \"#{nodule}\" not found"
       end
@@ -15,23 +15,6 @@ module Atheneum
     def self.constantize(string)
       string.to_s.split('_').map(&:capitalize).join
     end
-
-    def initialize(specific)
-      @specific = specific
-    end
-
-    def pack(item)
-      @specific.pack item
-    end
-
-    def unpack(item)
-      @specific.unpack item
-    end
-
-    def prefix
-      @specific.prefix
-    end
-
 
   end
 end
