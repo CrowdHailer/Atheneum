29ee384353dcdbf5c6c9da9058aea35378e0b5ab
Peter <peterhsaxton@gmail.com>
2014-12-12 08:52:48 +0000

    reverses password into storage

--- lib/atheneum.rb
+++ lib/atheneum.rb
@@ -2,7 +2,21 @@ require "atheneum/version"
 require "atheneum/errors"
 
 module Atheneum
+  class Strategy
+    module Reverse
+      def password=(password)
+        self.reversed_password = password.reverse
+      end
+    end
+  end
+end
+
+module Atheneum
   def self.method_missing(method_name, *arguments, &block)
-    raise StrategyUndefined.new "Strategy \"#{method_name}\" not found"
+    begin
+      Strategy.const_get method_name.capitalize
+    rescue NameError
+      raise StrategyUndefined.new "Strategy \"#{method_name}\" not found"
+    end
   end
 end
--- test/atheneum/atheneum_test.rb
+++ test/atheneum/atheneum_test.rb
@@ -7,4 +7,12 @@ class AtheneumTest < MiniTest::Test
     end
     assert_match(/no_strategy/, err.message)
   end
+
+  def test_can_store_a_password_reversed
+    dummy_class = Struct.new(:reversed_password)
+    dummy_class = dummy_class.include Atheneum.reverse :password
+    store = dummy_class.new
+    store.password = 'password'
+    assert_equal 'password'.reverse, store.reversed_password
+  end
 end
